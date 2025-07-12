f824464c8dd8533861bb9a15e57a464d29e45aa7
Manuel Sanchez <livediang@gmail.com>
2025-07-11 23:46:35 -0500

    Creacion Modulo de Administracion Users - Reset Password Method

--- Intranet.Web/Controllers/AccountController.cs
+++ Intranet.Web/Controllers/AccountController.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Intranet.Web.Models;
-using System.Security.Claims;
+﻿using Intranet.Web.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Claims;
 
 public class AccountController : Controller
 {
     private readonly AuthService _authService;
-    public AccountController(AuthService authService)
+    private readonly AppDbContext _context;
+    private readonly IConfiguration _configuration;
+    public AccountController(AuthService authService, AppDbContext context, IConfiguration configuration)
     {
         _authService = authService;
+        _context = context;
+        _configuration = configuration;
     }
 
     [HttpGet]
@@ -41,7 +48,86 @@ public class AccountController : Controller
         return View();
     }
 
+    [HttpGet]
+    public IActionResult LoginReset() => View();
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult LoginReset(string mailUser)
+    {
+        var userReset = _context.admUsers.FirstOrDefault(u => u.mailUser == mailUser);      
+        if(userReset == null)
+        {
+            ViewBag.Message = "Email not register.";
+            return View();
+        }
+
+        string token = Guid.NewGuid().ToString();
+        userReset.PasswordResetToken = token;
+        userReset.TokenExpiration = DateTime.Now.AddHours(1);
+        _context.SaveChanges();
+
+        string resetLink = Url.Action("LoginURL", "Account", new { token = token }, protocol: Request.Scheme);
+
+        try
+        {
+            var smtpUser = _configuration["SMTP:Email"];
+            var smtpPass = _configuration["SMTP:Password"];
+            var smtp = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(smtpUser, smtpPass),
+                EnableSsl = true
+            };
+
+            var message = new MailMessage(smtpUser, userReset.mailUser)
+            {
+                Subject = "Password Reset",
+                Body = $"URL Link to reset Password:\n{resetLink}",
+                IsBodyHtml = false
+            };
+
+            smtp.Send(message);
+
+            ViewBag.Message = "A link has been sent to your email to reset your password.";
+        }
+        catch (Exception ex)
+        {
+            ViewBag.Message = "Error to send message: " + ex.Message;
+        }
+
+        return View();
+    }
+
+    public IActionResult LoginURL(string token)
+    {
+        var emailUserToken = _context.admUsers.FirstOrDefault(u => u.PasswordResetToken == token && u.TokenExpiration > DateTime.Now);
+        if (emailUserToken == null)
+        {
+            return NotFound("Token invalid or expired.");
+        }
+
+        ViewBag.Token = token;
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult LoginURL(string token, string newPassword)
+    {
+        var userToken = _context.admUsers.FirstOrDefault(u => u.PasswordResetToken == token && u.TokenExpiration > DateTime.Now);
+        if (userToken == null)
+        {
+            return NotFound("Token invalid or expired.");
+        }
+
+        userToken.passwordUser = BCrypt.Net.BCrypt.HashPassword(newPassword);
+        userToken.PasswordResetToken = null;
+        userToken.TokenExpiration = null;
+        _context.SaveChanges();
+
+        ViewBag.Message = "Your password has been restablished.";
+        return View("ConfirmedReset");
+    }
 
     public async Task<IActionResult> Logout()
     {
@@ -49,8 +135,5 @@ public class AccountController : Controller
         return RedirectToAction("Login");
     }
 
-    public IActionResult AccessDenied()
-    {
-        return View("AccessDenied");
-    }
+    public IActionResult AccessDenied() => View("AccessDenied");
 }
